138ed1b228605305de3da3f37c6d7611e5e3a2e4
zhengvincent <vincentz2006@gmail.com>
2022-10-20 17:56:00 -0400

    Login page and update for index & JS

--- VincentZhengA1/scripts/main.js
+++ VincentZhengA1/scripts/main.js
@@ -1,2 +1,34 @@
-1
-// JavaScript source code
+// This function runs when the login button (on the login page) is pressed.
+// The function checks the login values from the textboxes and see if it matches the correct
+// credentials.
+function checkLogin() {
+    // Obtain the username and password values from the textboxes
+    let username = document.getElementById('usernameTextbox').value;
+    let password = document.getElementById('passwordTextbox').value;
+
+    // Check for correct credentials (username and password)
+    if (username == "vi" && password == "zh") {
+        // If login credentials are correct, the link to Massage Mayhem is shown and is set to index.html
+        document.getElementById("massageLink").hidden = false;
+        document.getElementById("massageLink").href = "index.html"
+    } else {
+        // Alert the user of their wrong credentials
+        alert("Wrong username or password")
+    }
+}
+
+// Set the sections of the back as constants
+const LOWER_BACK = 0;
+const MIDDLE_BACK = 1;
+const UPPER_BACK = 2;
+
+// Setting randomNumber as a global variable
+let randomNumber;
+
+// Store the user's information
+let count = 0;
+
+//
+function choosingBackPart() {
+
+}
